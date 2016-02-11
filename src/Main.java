7683be0e239959ee35384d039c1a65048d1713fb
carolineevail <carolineevail@gmail.com>
2016-02-11 09:23:09 -0500

    Initial Import. Created Main and Song classes.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by Caroline on 2/11/16.
+ */
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+/*Description
+
+    Write a program that asks the user to enter information about a particular good or service (it could be an album, game, book, car, etc). Ask for at least five different attributes, like title, author, and genre. Save the information to a file as a JSON object. When the program runs again, it should read the file and print out the data, then give the user the option to update it.
+
+        Requirements
+
+        Prompt the user to answer at least five different questions about a good or service.
+        Save the information into a JSON object in a file.
+        When the program first boots, read the file and display the contents.
+        Give the user the ability to update the information and re-save it to the file.
+        If the JSON parsing fails, give a friendly error rather than crashing the program.*/
---
+++ src/Song.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Caroline on 2/11/16.
+ */
+public class Song {
+    String songName;
+    String artist;
+    String album;
+    String genre;
+    int releaseYear;
+}
